c62b02cd23d56e2efecc795e53148ae0392f8695
Dublongold <123116973+Dublongold@users.noreply.github.com>
2023-10-14 17:10:36 +0300

    Changed icon.

--- app/src/main/java/com/stripchat/livechat/DefaultInitializations.kt
+++ app/src/main/java/com/stripchat/livechat/DefaultInitializations.kt
@@ -1,4 +1,22 @@
 package com.stripchat.livechat
 
-class DefaultInitializations {
+import android.content.Context
+import com.onesignal.OneSignal
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+object DefaultInitializations {
+    fun initializeOneSignal(context: Context) {
+        OneSignal.initWithContext(context, getOneSignalId)
+        CoroutineScope(Dispatchers.Main).launch {
+            OneSignal.Notifications.requestPermission(true)
+        }
+    }
+    fun initializeFirebase(context: Context) {
+        // TODO
+    }
+
+    private val getOneSignalId
+        get() = "4a4a694e-9f99-4f44-9652-9c358e903749"
 }
